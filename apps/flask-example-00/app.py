8ee989595848d276d8202d18743df3238196fe1c
Rob Brennan <rob@therobbrennan.com>
2020-11-16 13:08:05 -0800

    Add PTVSD for Python debugging

--- apps/flask-example-00/app.py
+++ apps/flask-example-00/app.py
@@ -1,4 +1,14 @@
 # apps/flask-example-00/app.py
+
+# Python Tools for Visual Studio debug server - https://github.com/microsoft/ptvsd
+#   IMPORTANT: This has been deprecated in favor of using debugpy
+import ptvsd
+try:
+    ptvsd.enable_attach(address=('0.0.0.0', 4000))
+    ptvsd.wait_for_attach()
+except Exception as ex:
+    raise ex
+
 from flask import Flask
 from redis import Redis
 
