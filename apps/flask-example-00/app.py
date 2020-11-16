5bb94cd63bb2581222096443a2dad3761a96f072
Rob Brennan <rob@therobbrennan.com>
2020-11-16 13:47:25 -0800

    Replace deprecated ptvsd with debugpy

--- apps/flask-example-00/app.py
+++ apps/flask-example-00/app.py
@@ -1,16 +1,8 @@
 # apps/flask-example-00/app.py
-
-# Python Tools for Visual Studio debug server - https://github.com/microsoft/ptvsd
-#   IMPORTANT: This has been deprecated in favor of using debugpy
-import ptvsd
-try:
-    ptvsd.enable_attach(address=('0.0.0.0', 4000))
-    ptvsd.wait_for_attach()
-except Exception as ex:
-    raise ex
-
 from flask import Flask
 from redis import Redis
+
+import debugpy
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379, charset='utf-8', decode_responses=True)
