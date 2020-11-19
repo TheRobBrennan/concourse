8c151a67ddb42b3161567c1ce122da026a0c6828
Rob Brennan <rob@therobbrennan.com>
2020-11-19 15:59:34 -0800

    Add @storybook/addon-actions

--- apps/nextjs-typescript-example-00/app/components/ColorButton.stories.tsx
+++ apps/nextjs-typescript-example-00/app/components/ColorButton.stories.tsx
@@ -1,6 +1,10 @@
 import React from "react"
+
+import { action } from "@storybook/addon-actions"
 import { storiesOf } from "@storybook/react"
+
 import ColorButton from "./ColorButton"
+
 storiesOf("ColorButton", module)
-  .add("red", () => <ColorButton color="red" />)
-  .add("blue", () => <ColorButton color="blue" />)
+  .add("red", () => <ColorButton color="red" onClick={action("clicked")} />)
+  .add("blue", () => <ColorButton color="blue" onClick={action("clicked")} />)
