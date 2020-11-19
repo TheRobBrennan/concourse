5315798a14fd590f82ef59d40896cf48971662e8
Rob Brennan <rob@therobbrennan.com>
2020-11-19 15:55:06 -0800

    Add @storybook/addon-controls

--- apps/nextjs-typescript-example-00/app/.storybook/main.js
+++ apps/nextjs-typescript-example-00/app/.storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ["../components/**/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-controls",
+  ],
 }
