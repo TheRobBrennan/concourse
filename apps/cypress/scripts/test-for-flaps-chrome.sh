cac625a7ef07b89622758312f932a0b78d348e42
Rob Brennan <rob@therobbrennan.com>
2020-11-19 23:16:44 -0800

    Create a Chrome version of the script to test for flaps

---
+++ apps/cypress/scripts/test-for-flaps-chrome.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# This handy script written by Rob Brennan <rob@therobbrennan.com>
+# is a great way to check a single Cypress test for flaps.
+#
+# If you discover certain Cypress tests are inconsistently failing, this is
+# a terrific way to verify whether you have resolved the problem or not.
+
+# Specify the full path to the test
+INT_TEST=${1:-cypress/integration/page.index.spec.js}
+
+# Specify the number of iterations to run
+ITERATIONS=${2:-100}
+
+# Initialize success and fail counters
+SUCCESS=0
+FAIL=0
+
+for i in $( seq 1 $ITERATIONS ); do
+    echo "Running $i of $ITERATIONS"
+
+    # Local development
+    # npx cypress run --browser electron --spec $INT_TEST
+    npx cypress run --browser chrome --spec $INT_TEST
+
+    rc=$?;
+
+    # Update our counters
+    if [ $rc != 0 ]
+      then # Failed run
+        echo "FAILED - Exit code: $rc"
+        FAIL=`expr $FAIL + 1`
+      else # Exit code of 0 is a success
+        SUCCESS=`expr $SUCCESS + 1`
+    fi
+
+done;
+
+# Calculate our success percentage
+SUCCESS_PCT_DISPLAY=$(echo "scale=2; ($SUCCESS/$ITERATIONS)*100" | bc)
+
+echo "\n\nCOMPLETE:\n\t$SUCCESS successes with $FAIL failure(s) over $ITERATIONS iterations\n\t$SUCCESS_PCT_DISPLAY % success rate\n"
+
