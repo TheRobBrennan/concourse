f483b1737a6ecb11b0b3cd939aaec12c5d0a25cc
Rob Brennan <rob@therobbrennan.com>
2020-11-19 16:08:04 -0800

    Add Storybook knobs to ColorButton stories

--- apps/nextjs-typescript-example-00/app/components/ColorButton.stories.tsx
+++ apps/nextjs-typescript-example-00/app/components/ColorButton.stories.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 
 import { action } from "@storybook/addon-actions"
+import { select, text, withKnobs } from "@storybook/addon-knobs"
 import { storiesOf } from "@storybook/react"
 
 import ColorButton from "./ColorButton"
 
 storiesOf("ColorButton", module)
-  .add("red", () => <ColorButton color="red" onClick={action("clicked")} />)
-  .add("blue", () => <ColorButton color="blue" onClick={action("clicked")} />)
+  .addDecorator(withKnobs)
+  .add("red", () => (
+    <ColorButton
+      color={select("color", { Red: "red", Dark: "darkred" }, "red")}
+      onClick={action("clicked")}
+    />
+  ))
+  .add("blue", () => (
+    <ColorButton color={text("color", "blue")} onClick={action("clicked")} />
+  ))
