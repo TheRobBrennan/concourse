2f8be7e188ae87af6e2678b7f024c85b5cd53ff2
Rob Brennan <rob@therobbrennan.com>
2020-11-20 00:10:37 -0800

    Added timing for script execution

--- apps/cypress/scripts/test-for-flaps.sh
+++ apps/cypress/scripts/test-for-flaps.sh
@@ -5,11 +5,14 @@
 # If you discover certain Cypress tests are inconsistently failing, this is
 # a terrific way to verify whether you have resolved the problem or not.
 
+# Start timing execution of our script
+start=`date +%s`
+
 # Specify the full path to the test
 INT_TEST=${1:-cypress/integration/page.index.spec.js}
 
 # Specify the number of iterations to run
-ITERATIONS=${2:-100}
+ITERATIONS=${2:-2}
 
 # Initialize success and fail counters
 SUCCESS=0
@@ -40,3 +43,7 @@ SUCCESS_PCT_DISPLAY=$(echo "scale=2; ($SUCCESS/$ITERATIONS)*100" | bc)
 
 echo "\n\nCOMPLETE:\n\t$SUCCESS successes with $FAIL failure(s) over $ITERATIONS iterations\n\t$SUCCESS_PCT_DISPLAY % success rate\n"
 
+# Stop timing execution of our script
+end=`date +%s`
+elapsedtime=$(($end-$start))
+echo "$(($elapsedtime / 60)) minutes and $(($elapsedtime % 60)) seconds elapsed for script execution. 🤓\n"
