cdc6e278b02c8ce7c49727a57d6fac2ddb7e4501
Rob Brennan <rob@therobbrennan.com>
2020-11-30 16:15:07 -0800

    Add getPostData and reference comments

--- apps/nextjs-blog/app/lib/posts.js
+++ apps/nextjs-blog/app/lib/posts.js
@@ -29,6 +29,20 @@ export function getAllPostIds() {
   })
 }
 
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, "utf8")
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents)
+
+  // Combine the data with the id
+  return {
+    id,
+    ...matterResult.data,
+  }
+}
+
 export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
--- apps/nextjs-blog/app/pages/posts/[id].js
+++ apps/nextjs-blog/app/pages/posts/[id].js
@@ -1,15 +1,49 @@
 import Layout from "../../components/layout"
 
-import { getAllPostIds } from "../../lib/posts"
+import { getAllPostIds, getPostData } from "../../lib/posts"
 
-export default function Post() {
-  return <Layout>...</Layout>
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
 }
 
+// getStaticPaths will return an array of possible values for id
 export async function getStaticPaths() {
   const paths = getAllPostIds()
+
+  // paths will be an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+
   return {
     paths,
     fallback: false,
+  }
+}
+
+// getStaticProps will fetch necessary data for the post with the supplied parameters
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData,
+    },
   }
 }
