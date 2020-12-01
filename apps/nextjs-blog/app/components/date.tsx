8f4ec5df8dc4dce5d81149ff53c1267864da8cab
Rob Brennan <rob@therobbrennan.com>
2020-11-30 17:48:47 -0800

    Convert to TypeScript 🤓

--- apps/nextjs-blog/app/components/date.tsx
+++ apps/nextjs-blog/app/components/date.tsx
@@ -1,11 +1,9 @@
 import { parseISO, format } from "date-fns"
 
-export default function Date({ dateString }) {
+export default function Date({ dateString }: { dateString: string }) {
   const date = parseISO(dateString)
 
   // See https://date-fns.org/v2.16.1/docs/format for more format() string options
   // Monday, November 30th, 2020
-  return (
-    <time dateTime={dateString}>{format(date, "EEEE, LLLL do, yyyy")}</time>
-  )
+  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>
 }
